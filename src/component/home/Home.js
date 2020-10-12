3b8a1d83f62982c8c3d5bcda4f9b3f4bc2d7c3dc
Daniel Nilsen <danieleidenilsen@hotmail.com>
2020-10-12 21:22:55 +0200

    update#

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -4,9 +4,7 @@ import CallOnHotel from "./HotelCards";
 function HomePage() { 
     return (
         <div className = "heroSection">
-        <div className = "heroText">
-            
-        </div>
+        <div className = "heroText"></div>
         <div className = "innerBox innerBox_frontPage">
          <h1 className = "heroText__text">Find your booking on Holidaze</h1>
 
